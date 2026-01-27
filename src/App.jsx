7904c60dc9e8006fb7e6bf64391cc6cf8be8366f
brightLight7 <mrabdullahsajid88@gmail.com>
2026-01-27 12:40:46 +0000

    Some changes made....

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Component from "./pages/template";
+
+function App() {
+  // Initialisation --------
+  // State -----------------
+  // Handlers --------------
+  // View ------------------
+  return <Component />;
+}
+
+export default App;
